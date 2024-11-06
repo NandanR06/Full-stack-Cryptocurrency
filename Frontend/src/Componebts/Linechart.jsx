663283520f5b5dc4bca6110ab263daaf06b04373
unknown <nandan6102003@gmail.com>
2024-11-06 16:48:17 +0530

    new file

--- Frontend/src/Componebts/Linechart.jsx
+++ Frontend/src/Componebts/Linechart.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect ,useState} from 'react'
 import chart from 'react-google-charts'
 
 export default function Linechart({historicalData}) {
