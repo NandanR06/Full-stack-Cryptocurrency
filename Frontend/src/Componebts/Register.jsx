d0b3079876843b6d8f762285314d6f2b70af6ec0
Nandan R <nandan6102003@gmail.com>
2024-11-06 19:08:00 +0530

    Update Register.jsx

--- Frontend/src/Componebts/Register.jsx
+++ Frontend/src/Componebts/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useState ,useEffect} from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 import './Register.css'
 import { useNavigate } from 'react-router-dom';
@@ -55,15 +55,7 @@ export default function Register() {
     }
   }
 
-  useEffect(()=>{
-    const disableRightClick=(e)=>{
-      e.preventDefault();
-    }
-     document.addEventListener("contextmenu",disableRightClick);
-     return()=>{document.removeEventListener('contextmenu',disableRightClick)};
-  },[])
-
-
+ 
   
   return (
     <div className='register'>
